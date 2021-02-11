efafa9a4e9c6a3cd80ed3ed106879cfe95b87e90
Joshuha Owen <joowen@microsoft.com>
2021-02-10 23:38:40 -0600

    Synapse Artificat upload + storage perms

--- deploy/Deployment.ps1
+++ deploy/Deployment.ps1
@@ -1,6 +1,6 @@
 ﻿#configurations - FILL OUT WITH DESIRED VALUES
 $dir = "C:\GitHub\SynapseServerlessImporter\deploy"     #Working directory of where your Deployment.ps1 file is located
-$resourceGroupName = "ssi-rg-test5"                     #Name of Azure resource group to deploy the lab resrouces to, will create if it does not exist
+$resourceGroupName = "ssi-rg-test2"                     #Name of Azure resource group to deploy the lab resrouces to, will create if it does not exist
 $location = "East US 2"                                 #Geo location of resource group, resources will use this as well
 $resourceNamePrefix = "ssi"                             #prefix to append on to unique names such as Synapse Workspace and Storage account
 $subscriptionName = "Visual Studio Premium with MSDN"               #Name of subscription to use for deployment (in case you want to deploy not to your default subscription)
@@ -78,8 +78,22 @@ $dbConn.Close()
 #run SQL script to create SPs on ServerlessDB
 $ConnectionStringServerless = "Server=$($ARMoutput.Outputs.synapseName.Value)-ondemand.sql.azuresynapse.net;Database=$serverlessDatabaseName;User ID=$username;Password=$passwd"
 
-$SSmaster = ConvertTo-SecureString -String $ConnectionStringMaster -AsPlainText -Force
 $SSserverless = ConvertTo-SecureString -String $ConnectionStringServerless -AsPlainText -Force
 
 #add key vault secret for Serverless DB
 Set-AzKeyVaultSecret -VaultName $($ARMoutput.Outputs.keyvaultName.Value) -Name "SSI-ServerlessDB" -SecretValue $SSserverless
+
+#load Synapse Workspace objects
+#replace key vault URL
+((Get-Content -path "$dir\workspaceArtifacts\linkedService\LS_AKV.json" -Raw) -replace '#AKVURL#', $($ARMoutput.Outputs.keyvaultName.Value)) | Set-Content -Path "$dir\workspaceArtifacts\linkedService\LS_AKV_Updated.json"
+#replace ADLS URL
+((Get-Content -path "$dir\workspaceArtifacts\linkedService\LS_Synapse_ADLS.json" -Raw) -replace '#ADLSURL#', $($ARMoutput.Outputs.storageName.Value)) | Set-Content -Path "$dir\workspaceArtifacts\linkedService\LS_Synapse_ADLS_Updated.json"
+Set-AzSynapseLinkedService -WorkspaceName $($ARMoutput.Outputs.synapseName.Value) -Name "LS_AKV" -DefinitionFile "$dir\workspaceArtifacts\linkedService\LS_AKV_Updated.json"
+Set-AzSynapseLinkedService -WorkspaceName $($ARMoutput.Outputs.synapseName.Value) -Name "LS_Synapse_ADLS" -DefinitionFile "$dir\workspaceArtifacts\linkedService\LS_Synapse_ADLS_Updated.json"
+Set-AzSynapseLinkedService -WorkspaceName $($ARMoutput.Outputs.synapseName.Value) -Name "LS_HTTP" -DefinitionFile "$dir\workspaceArtifacts\linkedService\LS_HTTP.json"
+Set-AzSynapseLinkedService -WorkspaceName $($ARMoutput.Outputs.synapseName.Value) -Name "LS_AzSQL_ServerlessPool" -DefinitionFile "$dir\workspaceArtifacts\linkedService\LS_AzSQL_ServerlessPool.json"
+Set-AzSynapseDataset -WorkspaceName $($ARMoutput.Outputs.synapseName.Value) -Name "DS_HTTP_CSV_Generic" -DefinitionFile "$dir\workspaceArtifacts\dataset\DS_HTTP_CSV_Generic.json"
+Set-AzSynapseDataset -WorkspaceName $($ARMoutput.Outputs.synapseName.Value) -Name "DS_ADLS_CSV_Generic" -DefinitionFile "$dir\workspaceArtifacts\dataset\DS_ADLS_CSV_Generic.json"
+Set-AzSynapsePipeline -WorkspaceName $($ARMoutput.Outputs.synapseName.Value) -Name "3-SQL Serverless Data Layer" -DefinitionFile "$dir\workspaceArtifacts\pipeline\3-SQL Serverless Data Layer.json"
+Set-AzSynapsePipeline -WorkspaceName $($ARMoutput.Outputs.synapseName.Value) -Name "2-HTTP CSV to ADLS CSV" -DefinitionFile "$dir\workspaceArtifacts\pipeline\2-HTTP CSV to ADLS CSV.json"
+Set-AzSynapsePipeline -WorkspaceName $($ARMoutput.Outputs.synapseName.Value) -Name "1-Main Orchestrator" -DefinitionFile "$dir\workspaceArtifacts\pipeline\1-Main Orchestrator.json"
